f04be64586090a845ab578296207595fe67fe6b8
recep-repo <recepedurmus@gmail.com>
2022-01-17 01:15:26 +0300

    v3

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -4,12 +4,12 @@ import { getDatabase, ref, set, push, onValue, query } from "firebase/database";
 import { useState, useEffect } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJUq9egwp-vTbBtT1csjh_SGhWJbKDN_k",
-  authDomain: "fire-blog-69494.firebaseapp.com",
-  projectId: "fire-blog-69494",
-  storageBucket: "fire-blog-69494.appspot.com",
-  messagingSenderId: "439089959599",
-  appId: "1:439089959599:web:661b883d46a02747d87a5a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
